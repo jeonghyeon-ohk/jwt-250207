f8d0176af90f0323971ddf05ab0a023e2853797c
jeonghyeon-ohk <ok6737@naver.com>
2025-02-10 11:20:53 +0900

    login response accessToken

--- src/main/java/com/example/jwt/standard/util/Ut.java
+++ src/main/java/com/example/jwt/standard/util/Ut.java
@@ -3,9 +3,9 @@ package com.example.jwt.standard.util;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 
 import javax.crypto.SecretKey;
-import java.security.Key;
 import java.util.Date;
 import java.util.Map;
 
@@ -24,7 +24,10 @@ public class Ut {
     }
 
     public static class Jwt {
-        public static String createToken(Key secretKey, int expireSeconds, Map<String, Object> claims) {
+        public static String createToken(String keyString, int expireSeconds, Map<String, Object> claims) {
+
+            SecretKey secretKey = Keys.hmacShaKeyFor(keyString.getBytes());
+
             Date issuedAt = new Date();
             Date expiration = new Date(issuedAt.getTime() + 1000L * expireSeconds);
 
@@ -34,30 +37,42 @@ public class Ut {
                     .expiration(expiration)
                     .signWith(secretKey)
                     .compact();
+
             return jwt;
         }
 
-        public static boolean isValidToken(SecretKey secretKey, String token) {
+        public static boolean isValidToken(String keyString, String token) {
             try {
+
+                SecretKey secretKey = Keys.hmacShaKeyFor(keyString.getBytes());
+
                 Jwts
                         .parser()
                         .verifyWith(secretKey)
                         .build()
                         .parse(token);
 
-                return true;
             } catch (Exception e) {
                 return false;
             }
+
+            return true;
+
         }
 
-        public static Map<String, Object> getPayload(SecretKey secretKey, String jwtStr) {
+
+
+        public static Map<String, Object> getPayload(String keyString, String jwtStr) {
+
+            SecretKey secretKey = Keys.hmacShaKeyFor(keyString.getBytes());
+
             return (Map<String, Object>) Jwts
                     .parser()
                     .verifyWith(secretKey)
                     .build()
                     .parse(jwtStr)
                     .getPayload();
+
         }
     }
 }
